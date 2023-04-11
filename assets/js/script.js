2a1757142e56e4000a56c083ed0a2b82c34c9961
Jesse Emerson <jesseemerson7@gmail.com>
2023-04-10 21:26:50 -0400

    added fetch code for trip advisor api

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+
+// ****This fetch for location id number by city name
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'b409b7abe1mshd693097ae9fc635p1c58ffjsn0debf85713b0',
+// 		'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
+// 	}
+// };
+
+
+// ****This fetch for location restaurants list of 30
+
+// fetch('https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation?query=Orlando', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "b409b7abe1mshd693097ae9fc635p1c58ffjsn0debf85713b0",
+//     "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com",
+//   },
+// };
+
+// fetch(
+//   "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=34515",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));

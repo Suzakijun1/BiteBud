c50220568f1509c7430d22e4da8cfd4d1c11ed9b
trentstanley92 <trentstanley92@gmail.com>
2023-04-11 10:33:52 -0400

    adds fetch for restaurant list by 30

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,28 +26,30 @@ function toggleDropdown(event) {
 // };
 
 
-// ****This fetch for location restaurants list of 30
-
 // fetch('https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation?query=Orlando', options)
 // 	.then(response => response.json())
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
 
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "b409b7abe1mshd693097ae9fc635p1c58ffjsn0debf85713b0",
-//     "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com",
-//   },
-// };
+// ****This fetch for location restaurants list of 30
 
-// fetch(
-//   "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=34515",
-//   options
-// )
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
+const cityName = 'Orlando'; // Replace with function for user inputted cityName
+
+// Fetch restaurant list by city name
+fetch(`https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation?query=${cityName}`, {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'b409b7abe1mshd693097ae9fc635p1c58ffjsn0debf85713b0',
+    'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
+  }
+})
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+})
+.catch(error => console.error(error));
+
+
 
 
 // const options = {

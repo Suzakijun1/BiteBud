6938c484c9450a4d38ad9140311722d748f79e76
Jesse Emerson <jesseemerson7@gmail.com>
2023-04-11 19:19:16 -0400

    put existing youtube for loop into function. deleted some comments and added new comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var mainPage = document.querySelector("#main-page");
 var informationPage = document.querySelector("#search-page");
 var testing = document.querySelector("#mainPageInfo");
-
+//****dropdown logic for navbar
 function toggleDropdown(event) {
   event.preventDefault();
   var dropdownContent = event.target.nextElementSibling;
@@ -23,11 +23,10 @@ var searchInput = document.getElementById("inputMain");
 var searchBtn = document.getElementById("searchBtnMain");
 var formMain = document.getElementById("formMain");
 
-// ****This fetch for location id number by city name
-function fetchLocationId(e) {
-  // e.preventDefault();
-  locationInput = searchInput.value;
-  console.log(locationInput);
+// ****This fetch is for location id number by city name to be sent to fetchlocationid2 function to get location's restaurant list.
+function fetchLocationId() {
+  searchInput.value;
+  console.log(searchInput.value);
   const options = {
     method: "GET",
     headers: {
@@ -36,7 +35,7 @@ function fetchLocationId(e) {
     },
   };
   fetch(
-    `https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation?query=${locationInput}`,
+    `https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation?query=${searchInput.value}`,
     options
   )
     .then((response) => response.json())
@@ -47,56 +46,34 @@ function fetchLocationId(e) {
     })
     .catch((err) => console.error(err));
 }
-
+//**** search bar button event listener to start function chain and to hide main page
 searchBtn.addEventListener("click", function () {
   fetchLocationId();
   mainPage.classList.add("hidden");
   informationPage.classList.remove("hidden");
   testing.classList.add("hidden");
 });
+//**** function to grab info from trip advisor restaurant list and search videos for the top 5 on the list by name and city
+function fetchYoutubeVideo(restaurant, city) {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "cc6b170988mshf1759b35552ccc1p19f93ejsn2c2fce67394d",
+      "X-RapidAPI-Host": "youtube-data8.p.rapidapi.com",
+    },
+  };
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "b409b7abe1mshd693097ae9fc635p1c58ffjsn0debf85713b0",
-    "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com",
-  },
-};
-
-fetch(
-  "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=34515",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => {
-    console.log(response.data.data)
-    for(var i=0; i < 5; i++){
-      console.log(response.data.data[i])
-      console.log(response.data.data[i].name)
-      console.log(response.data.data[i].parentGeoName)
-      fetchYoutubeVideo(response.data.data[i].name,response.data.data[i].parentGeoName)
-    }
-  })
-  .catch((err) => console.error(err));
-  function fetchYoutubeVideo(restaurant,city){
-  
-
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'cc6b170988mshf1759b35552ccc1p19f93ejsn2c2fce67394d',
-        'X-RapidAPI-Host': 'youtube-data8.p.rapidapi.com'
-      }
-    };
-  
-    fetch(`https://youtube-data8.p.rapidapi.com/search/?q=${restaurant} ${city}&hl=en&gl=US`, options)
-      .then(response => response.json())
-      .then(response => {
-        console.log(response.contents[0].video)
-      })
-      .catch(err => console.error(err));
-  }
-  
+  fetch(
+    `https://youtube-data8.p.rapidapi.com/search/?q=${restaurant} ${city}&hl=en&gl=US`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.contents[0].video);
+    })
+    .catch((err) => console.error(err));
+}
+//**** Grabs location ID from getlocationid function and GET's list of restaurants and sends that info to create cards function and fetchyoutubevideo function.
 function fetchLocationId2(functionid) {
   console.log(functionid);
   const options = {
@@ -112,14 +89,22 @@ function fetchLocationId2(functionid) {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      createCards(data.data.data);
+      console.log(data.data.data);
+      // createCards(data.data.data); *commented out for future changes
+      console.log(data.data.data);
+      for (var i = 0; i < 5; i++) {
+        console.log(data.data.data[i]);
+        console.log(data.data.data[i].name);
+        console.log(data.data.data[i].parentGeoName);
+        fetchYoutubeVideo(
+          data.data.data[i].name,
+          data.data.data[i].parentGeoName
+        );
+      }
     })
     .catch((err) => console.error(err));
 }
-
+//**** creates cards that contain restaurants pic name price range open status
 function createCards(restaurants) {
   restaurants.forEach((restaurant) => {});
 }
-
-

7b7f3603232f189f925d3d082516a8927161934f
Alan Pereyro <alanpereyro0@gmail.com>
2023-04-11 17:29:27 -0400

    youtube api is now pulling name of restuarant and location

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,48 +45,47 @@ function fetchLocationId(e) {
 
 searchBtn.addEventListener("click", fetchLocationId);
 
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "b409b7abe1mshd693097ae9fc635p1c58ffjsn0debf85713b0",
-//     "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com",
-//   },
-// };
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b409b7abe1mshd693097ae9fc635p1c58ffjsn0debf85713b0",
+    "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com",
+  },
+};
 
-// fetch(
-//   "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=34515",
-//   options
-// )
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
+fetch(
+  "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=34515",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response.data.data)
+    for(var i=0; i < 5; i++){
+      console.log(response.data.data[i])
+      console.log(response.data.data[i].name)
+      console.log(response.data.data[i].parentGeoName)
+      fetchYoutubeVideo(response.data.data[i].name,response.data.data[i].parentGeoName)
+    }
+  })
+  .catch((err) => console.error(err));
 
 
-// let searchInput = document.getElementById("inputMain");
-// let searchBtn = document.getElementById("searchBtnMain");
-// let formMain = document.getElementById("myForm");
+function fetchYoutubeVideo(restaurant,city){
+  
 
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'cc6b170988mshf1759b35552ccc1p19f93ejsn2c2fce67394d',
+      'X-RapidAPI-Host': 'youtube-data8.p.rapidapi.com'
+    }
+  };
 
-// function fetchYoutubeVideo(e){
-//   e.preventDefault();
-//   const youtubeVideo = searchInput.value;
-//   console.log(youtubeVideo);
-
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Key': 'cc6b170988mshf1759b35552ccc1p19f93ejsn2c2fce67394d',
-//       'X-RapidAPI-Host': 'youtube-data8.p.rapidapi.com'
-//     }
-//   };
-
-//   fetch(`https://youtube-data8.p.rapidapi.com/search/?q=${youtubeVideo}&hl=en&gl=US`, options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-// }
-
-// fetch('https://youtube-data8.p.rapidapi.com/search/?q=movie&hl=en&gl=US', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+  fetch(`https://youtube-data8.p.rapidapi.com/search/?q=${restaurant} ${city}&hl=en&gl=US`, options)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response.contents[0].video)
+    })
+    .catch(err => console.error(err));
+}
+// fetchYoutubeVideo();
